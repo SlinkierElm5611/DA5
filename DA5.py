08236df9fd2f58a98707b89d17605f83338b3908
stefanbalta <stefan.balta.sb@gmail.com>
2023-03-29 15:23:49 -0400

    added updated flex code

--- DA5.py
+++ DA5.py
@@ -3,13 +3,14 @@ import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
-FlexCode = """TITLE 
+FlexCode = """
+TITLE 
 'DA5'
 SELECT
 errlim=1e-3
-ngrid=1
+ngrid=5
 spectral_colors
-modes = 1
+modes = 3
 
 COORDINATES
 cartesian2
@@ -17,17 +18,24 @@ cartesian2
 VARIABLES
 u	!Displacement in x
 v !Displacement in y
-!w !Displacement in z
 
 DEFINITIONS
 mag=0.3*globalmax(magnitude(x,y))/globalmax(magnitude(u,v))
-Ly = %s ! length
-Lx = %s*(Ly)
-LxQz = Lx/(0.85+1) ! thickness
-LxTi = Lx-LxQz ! thickness h/(m+1) = a2
-LyAu = %s*Ly ! window length
-LxAu = %s ! window thickness
+
+! Dimensions
+Ly = %s! length (0.5mm - 1 cm)
+LxTotal = %s*Ly ! total thickness 
+LyAu = %s*Ly ! window length 
+
+
 Lz = Ly/4 ! width
+
+m = 1.22 !0.8595113554
+{LxQz = LxTotal/(m+1) ! thickness of quartz
+LxTi = LxTotal - LxQz ! thickness of titanium}
+LxTi = LxTotal/(m+1) ! thickness of quartz
+LxQz = LxTotal - LxTi ! thickness of titanium
+LxAu = %s ! window thickness
 
 ! Material Properties
 rho
@@ -35,6 +43,7 @@ E = 0
 nu = 0
 G=E/(2*(1+nu))
 
+{ Stiffness matrix }
 C11 =E*(1-nu)/(1+nu)/(1-2*nu)
 C12 = E*nu/(1+nu)/(1-2*nu)	C13 = C12	C14=0	C15=0	C16=0
 C21 = C12	C22 = C11	C23 = C12	C24=0 C25=0 C26=0
@@ -43,12 +52,27 @@ C41=0 C42=0 C43=0 C44=G C45=0 C46=0
 C51=0 C52=0 C53=0 C54=0 C55=G C56=0
 C61=0 C62=0 C63=0 C64=0 C65=0 C66=G
 
+{ Piezoelectric coupling coefficients }
+d11 = 20e-12            d12 = -24e-12		d13 = 0             d14 = 0 d15 = 0 d16 = 0
+d21 = 16e-12            d22 = -35e-12		d23 = 0             d24 = 0 d25 = 0 d26 = 0
+d31 = 45e-12            d32 = 0				d33 = 70e-12    d34 = 0 d35 = 0 d36 = 0
+
+{ Electric field }
+voltage = 1e3
+Efieldx = -voltage/LxQz
+Efieldy = 0
+Efieldz = 0
+
 !! Strain
 !Axial Strain
-ex=dx(u) !partial derivatives
+ex=dx(u)
 ey=dy(v)
 !Engineering Shear Strain
 gxy=(dx(v)+dy(u))
+!Mechanical strain
+exm  = ex  - (d11*Efieldx+d21*Efieldy+d31*Efieldz)
+eym  = ey  - (d12*Efieldx+d22*Efieldy+d32*Efieldz)
+gxym = gxy - (d16*Efieldx+d26*Efieldy+d36*Efieldz)
 
 !!Stress via Hooke's law
 !Axial Stress
@@ -58,8 +82,7 @@ sy = C21*ex+C22*ey+C26*gxy
 !Shear stress
 sxy=C61*ex+C62*ey+C66*gxy
 
-omega = sqrt(lambda)
-f = 2*pi*omega
+ f = sqrt(lambda)/2*pi
 
 EQUATIONS
 !FNet = 0
@@ -71,38 +94,38 @@ BOUNDARIES
 	    rho = 4.506e3
 		E = 106e9
         nu = 0.34
-    	START(0,-Ly/2) !y=0 surface:
-		value(u)=0
+    	START(0,0) !y=0 surface:
+		load(u)=0
 		value(v)=0
-    	LINE TO (LxTi,-Ly/2) !x=Lx surface
+    	LINE TO (-LxTi,0) !x=Lx surface
 		load(u)=0
 		load(v)=0
-		LINE TO (LxTi,Ly/2) !y=Ly surface
+		LINE TO (-LxTi,Ly/2) !y=Ly surface
 		value(u)=0
-		value(v)=0
+		value(v)=0 
 		LINE TO (0,Ly/2) !x=0 surface
 		load(u)=0
 		load(v)=0
 		LINE TO CLOSE
         
     REGION 2 'Quartz'
-    	rho = 2.684
+    	rho = 2.684e3
 		C11 = 8.67361769904436e10	C12 = 6.98526725701223e9	C13 = 1.19104335397808e10	C14 = 1.79081384131957e10	C15=0	C16=0
 		C21 = 6.98526725701223e9	C22 = 8.67361769904436e10	C23 = 1.19104335397808e10	C24=-1.79081384131957e10	C25=0 C26=0
 		C31 = 1.19104335397808e10	C32 = 1.19104335397808e10	C33 = 1.07193901858028e11	C34=0 C35=0 C36=0
 		C41=1.79081384131957e10	C42 = -1.79081384131957e10	C43=-2.57028375615534e-7 C44=5.79427767324731e10 C45=0 C46=0
 		C51=0 C52=0 C53=0 C54=0 C55=5.79491958802304e10 C56=1.79224317155352e10
 		C61=0 C62=0 C63=0 C64=0 C65=1.79224317155352e10 C66=3.99072812865916e10
-    	START(LxTi,-Ly/2) !y=0 surface:
-		value(u)=0
+    	START(0,0) !y=0 surface:
+		load(u)=0
 		value(v)=0
-    	LINE TO (LxTi+LxQz,-Ly/2) !x=Lx surface
+    	LINE TO (LxQz,0) !x=Lx surface
 		load(u)=0
 		load(v)=0
-		LINE TO (LxTi+LxQz,Ly/2) !y=Ly surface
-		value(u)=0
-		value(v)=0
-		LINE TO (LxTi,Ly/2) !x=0 surface
+		LINE TO (LxQz,Ly/2) !y=Ly surface
+        value(u)=0
+		value(v)=0 
+		LINE TO (0,Ly/2) !x=0 surface
 		load(u)=0
 		load(v)=0
 		LINE TO CLOSE
@@ -111,16 +134,16 @@ BOUNDARIES
     	rho = 19.3e3
         E = 79e9
 		nu = 0.42
-    	START(LxTi+LxQz,-LyAu/2) !y=0 surface:
-		value(u)=0
+    	START(LxQz,0) !y=0 surface:
+		load(u)=0
 		value(v)=0 
-    	LINE TO (LxTi+LxQz+LxAu,-LyAu/2) !x=Lx surface
+    	LINE TO (LxQz+LxAu,0) !x=Lx surface
 		load(u)=0
 		load(v)=0
-		LINE TO (LxTi+LxQz+LxAu,LyAu/2) !y=Ly surface
+		LINE TO (LxQz+LxAu,LyAu/2) !y=Ly surface
 		value(u)=0
-		value(v)=0
-		LINE TO (LxTi+LxQz,LyAu/2) !x=0 surface
+		value(v)=0 
+		LINE TO (LxQz,LyAu/2) !x=0 surface
 		load(u)=0
 		load(v)=0
 		LINE TO CLOSE
@@ -129,18 +152,17 @@ BOUNDARIES
 PLOTS
 	grid(x+u*mag, y+v*mag)
  	!contour(u) on surface z=0
-    !contour(sy) on surface y=0
-	!elevation(sx,sy,sz) from (0,0,0) to (0,0,Lz)
     elevation(u) from (LxTi+LxQz/2,-Ly/2) to (LxTi+LxQz/2, Ly/2)
 	
 	SUMMARY
-		export file 'DA4_summary.txt'
-		report f
-end"""
+        export file 'DA5_summary.txt'
+		    report f
+end
+"""
 # Path Variables
 FlexFileName = "DA5" 
-FlexLocation = "FlexPDE6s"
-FlexVersion = 6
+FlexLocation = "/Applications/FlexPDE7/FlexPDE7.app/Contents/MacOS/FlexPDE7"
+FlexVersion = 7
 TextFile = (FlexVersion==7)*("DA5_output/")+'DA5_summary.txt'
 
 def getSensitivity(dimensions):
@@ -153,7 +175,7 @@ def getSensitivity(dimensions):
         #print(FlexCode %(Lbridge, Wbridge, hbridge,Lwindow), file = f)
         print(FlexCode %(Lbridge, hpercent, lpercent, mat_th ), file = f)
     # Runs FlexPDE
-    subprocess.run([FlexLocation, "-S",FlexFileName],timeout=5)
+    subprocess.run([FlexLocation, "-S",FlexFileName])
 
     # Retrieves data
     with open(TextFile) as f:
